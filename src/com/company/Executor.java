2c4be8ddd25cb7271fbe1f276e293959bae2e962
Tanya Tarasovec <tanyashipicina300197@gmail.com>
2021-12-29 18:08:55 +0300

    collection

---
+++ src/com/company/Executor.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Executor {
+
+    public static void main(String[] args) {
+        List<String> ListStr = new ArrayList<String>();
+        ListStr.add("Football");
+        ListStr.add("Tennis");
+        ListStr.add("Voleyball");
+        ListStr.add("Basketball");
+        ListStr.add("Run");
+        System.out.println(ListStr.get(1));
+    }
+        }
+
+        //   for (String str1 : ListStr) {
+        //       System.out.println(str1);
+        //   }
+
+
+        //   }
+//}
+//              ListStr.remove(2);
+//             for (String str1 : ListStr) {
+//            System.out.println(str1);
+ //       }
+  //  }
+//}
